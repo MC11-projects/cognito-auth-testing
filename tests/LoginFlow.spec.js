27d6105b75d0df1407ad94ee9b2990bfa9d74378
MC11-projects <mariuscristianpopescu@yahoo.com>
2026-03-19 17:31:14 +0200

    Add Cognito Login Test with Playwright

---
+++ tests/LoginFlow.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv'
+dotenv.config()
+
+test('User Login', async({page}) => {
+    const email = process.env.TEST_EMAIL1
+    const password = process.env.TEST_PASSWORD1
+    const baseUrl = process.env.BASE_URL
+
+    await page.goto(baseUrl)
+    await page.getByText('Login with Cognito').click()
+    await page.getByRole('textbox', {name: 'Email address'}).fill(email)
+    await page.getByText('Next').click()
+    await page.getByRole('textbox', {name: 'Password'}).waitFor()
+    await page.getByRole('textbox', {name: 'Password'}).fill(password)
+    await page.getByText('Continue').click()
+
+    await expect(page.getByText('Access Token')).toBeVisible()
+
+    await page.getByText('Logout').click()
+    await expect(page.getByText('AWS Cognito Authentication Test')).toBeVisible()
+
+})
