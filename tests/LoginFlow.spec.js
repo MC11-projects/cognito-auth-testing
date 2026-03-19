444f3a48357bbc5c14eac7565ccc9e0b4dec25dc
MC11-projects <mariuscristianpopescu@yahoo.com>
2026-03-19 18:32:10 +0200

    Log debug

--- tests/LoginFlow.spec.js
+++ tests/LoginFlow.spec.js
@@ -7,11 +7,20 @@ test('User Login', async({page}) => {
     const password = process.env.TEST_PASSWORD1
     const baseUrl = process.env.BASE_URL
 
+    console.log('=== ENV VARIABLES ===');
+    console.log('Email:', email);
+    console.log('Password:', password ? 'SET' : 'UNDEFINED');
+    console.log('BaseUrl:', baseUrl);
+    console.log('=====================');
+
+
+
     await page.goto(baseUrl)
     await page.screenshot({ path: 'screenshot-1-homepage.png' })
     await page.getByText('Login with Cognito').click()
     await page.screenshot({ path: 'screenshot-2-after-click.png' })
     await page.getByRole('textbox', {name: 'Email address'}).waitFor({ timeout: 10000 })
+    console.log('About to fill email field with:', email);
     await page.getByRole('textbox', {name: 'Email address'}).fill(email)
     await page.screenshot({ path: 'screenshot-3-email-filled.png' })
     await page.getByText('Next').click()
