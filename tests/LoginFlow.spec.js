b97b29de68bd799ae17d717128a30441e7916167
MC11-projects <mariuscristianpopescu@yahoo.com>
2026-03-19 18:24:52 +0200

    Readded waitfor for both email and password

--- tests/LoginFlow.spec.js
+++ tests/LoginFlow.spec.js
@@ -11,10 +11,12 @@ test('User Login', async({page}) => {
     await page.screenshot({ path: 'screenshot-1-homepage.png' })
     await page.getByText('Login with Cognito').click()
     await page.screenshot({ path: 'screenshot-2-after-click.png' })
+    await page.getByRole('textbox', {name: 'Email address'}).waitFor({ timeout: 10000 })
     await page.getByRole('textbox', {name: 'Email address'}).fill(email)
     await page.screenshot({ path: 'screenshot-3-email-filled.png' })
     await page.getByText('Next').click()
     await page.screenshot({ path: 'screenshot-4-after-next.png' })
+    await page.getByRole('textbox', {name: 'Password'}).waitFor({ timeout: 10000 })
     await page.getByRole('textbox', {name: 'Password'}).fill(password)
     await page.getByText('Continue').click()
 
