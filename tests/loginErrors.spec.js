0f59a5639af252a58807ef2c0a753641b3798c47
MC11-projects <mariuscristianpopescu@yahoo.com>
2026-03-24 23:07:37 +0200

    Added negative tests on separate file

---
+++ tests/loginErrors.spec.js
@@ -0,0 +1,52 @@
+import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv' //enable only for local tests
+dotenv.config() //enable only for local tests
+
+test.beforeEach(async ({page}) => {
+    const baseUrl = process.env.BASE_URL
+    await page.goto(baseUrl)
+    await page.getByText('Login with Cognito').click()
+    await page.getByRole('textbox', {name: 'Email address'}).waitFor({ timeout: 10000 })
+})
+
+test('Empty Email field', async ({page}) => {
+    const email = process.env.TEST_EMAIL1
+    
+    await page.getByRole('textbox', {name: 'Email address'})
+    await page.getByText('Next').click()
+    await expect(page.getByText('Missing email address')).toBeVisible()
+
+})
+
+test('Empty Password field', async ({page}) => {
+    const email = process.env.TEST_EMAIL1
+    const password = process.env.TEST_PASSWORD1
+    
+    await page.getByRole('textbox', {name: 'Email address'}).fill(email)
+    await page.getByText('Next').click()
+    await page.getByRole('textbox', {name: 'Password'}).waitFor({ timeout: 10000 })
+    await page.getByRole('textbox', {name: 'Password'})
+    await page.getByText('Continue').click()
+    await expect(page.getByText('Missing password.')).toBeVisible()
+
+})
+
+test('Invalid Email', async ({page}) => {
+    await page.getByRole('textbox', {name: 'Email address'}).fill('test1')
+    await page.getByText('Next').click()
+    await expect(page.getByText('Invalid email address.')).toBeVisible()
+    await expect(page.getByText('Invalid input: Please check your input and try again.', {exact: true})).toBeVisible()
+    
+})
+
+test('Invalid Password', async ({page}) => {
+    const email = process.env.TEST_EMAIL1
+    
+    await page.getByRole('textbox', {name: 'Email address'}).fill(email)
+    await page.getByText('Next').click()
+    await page.getByRole('textbox', {name: 'Password'}).waitFor({ timeout: 10000 })
+    await page.getByRole('textbox', {name: 'Password'}).fill('password')
+    await page.getByText('Continue').click()
+    await expect(page.getByText('Invalid input: Incorrect username or password.', {exact: true})).toBeVisible()
+
+})
